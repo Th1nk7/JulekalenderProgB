0d32b66e05681f60d29902cba7a1cc1ed7350676
Th1nk <Tobiasransborg@hotmail.com>
2023-11-17 16:16:24 +0100

    basicallylaager

--- test2/sketch.js
+++ test2/sketch.js
@@ -1,38 +1,58 @@
-let size = 75;
+let size = 100;
 let isOpenArr = new Array();
+let isOpenPermArr = new Array();
 let degreesArr = new Array();
+let rotationStartedArr = new Array();
+let img1, img2, img3, img4;
 
 function preload(){
   font = loadFont('comicsans.ttf');
+  img1 = loadImage("jul1.png"); img2 = loadImage("jul2.png"); img3 = loadImage("jul3.png"); img4 = loadImage("jul4.png")
 }
 
 // Alt bag lågen skal laves efter
-// x, y, farve, tal
-function laageSetup(x, y, color, number){
-  let rotationDegrees = degreesArr[number-1];
-  let rotationStarted = false;
-  if (mouseX > x-75/2 && mouseX < x+75/2 && mouseY > y-75/2 && mouseY < y-75/2 && mouseIsPressed == true){
-    rotationStarted = true;
+// x, y, billede, tal, talXPosition, textColor
+function laageSetup(x, y, laageImage, number, textPos, textColor){
+  push()
+  let rotationStarted = rotationStartedArr[number-1];
+  if (mouseX > x-size/2+windowWidth/2 && mouseX < x+size/2+windowWidth/2 && mouseY > y-size/2+windowHeight/2 && mouseY < y+size/2+windowHeight/2 && mouseIsPressed == true){
+    rotationStartedArr[number-1] = true;
     console.log("clicked")
   }
   fill("black")
-  rect(x-75/2-windowWidth/2,y-75/2-windowHeight/2,size);
-  translate(-size,-size,0)
-  fill(color);
-  if(rotationDegrees < -160){
-    rotateY(rotationDegrees);
-    isOpenArr[number-1] = true;
+  rect(x-size/2,y-size/2,size);
+  translate(x-size/2,y-size/2,0)
+  fill("red");
+  rotateY(0)
+  if(degreesArr[number-1] < -160){
+    rotateY(degreesArr[number-1]);
+    if(isOpenPermArr[number-1] == false){
+      isOpenArr[number-1] = true;
+      isOpenPermArr[number-1] = true
+    }
+    else{
+      isOpenArr[number-1] = false;
+    }
   }
   else if (rotationStarted == true){
-    rotationDegrees --;
-    rotateY(rotationDegrees);
+    degreesArr[number-1] --;
+    rotateY(degreesArr[number-1]);
   }
-  rect(0, 0, size);
-  fill("black")
-  translate(0,0,1)
-  text("1",x,y+50)
+  image(laageImage,0,0,size,size)
+  translate(0,0,-1)
   fill("white")
-  degreesArr[number-1] = rotationDegrees;
+  rect(0,0,size-1)
+  fill(textColor)
+  translate(0,0,2)
+  text(number,textPos,80)
+  fill("white")
+  pop()
+}
+
+function isOpenChecker(number,link){
+  if(isOpenArr[number-1]==true){
+    window.open(link)
+  }
 }
 
 function setup() {
@@ -40,18 +60,26 @@ function setup() {
   textSize(70);
   angleMode(DEGREES);
   textFont(font)
+  noStroke()
 
   for(let i = 1; i < 25; i++){
     isOpenArr.push(false)
+    isOpenPermArr.push(false)
     degreesArr.push(0)
+    rotationStartedArr.push(false)
   }
 }
 
 function draw() {
   background(220);
-  fill("red")
-  rect(-50,-50,75)
-  laageSetup(20,10,"blue",1)
+  laageSetup(-700,-80,img1,1,32,"white")
+  laageSetup(400,250,img2,2,27,"white")
+  laageSetup(258,-312,img3,3,27,"white")
+  laageSetup(612,-111,img4,4,25,"white")
+  isOpenChecker(1,"")
+  isOpenChecker(2,"")
+  isOpenChecker(3,"")
+  isOpenChecker(4,"")
 }
 
 function windowResized(){
