be64ce0120cc1322420027098db6952222522dd1
Th1nk <Tobiasransborg@hotmail.com>
2023-12-06 13:11:45 +0100

    uhwduhwduh machine learniung

--- laage7/laage7.js
+++ laage7/laage7.js
@@ -1,42 +1,73 @@
-let faceAPI
-let detections = [];
 let video;
+let model
+let face
+let firstFace = true;
 
-function setup() {
-  createCanvas(windowWidth,windowHeight);
+
+function setup(){
+  createCanvas(640,480)
+  ellipse(width/2,height/2,100)
   video = createCapture(VIDEO)
-
-  video.size(height,height)
-  const faceOptions = { withLandmarks: true, withExpressions: false, withDescriptors: false }
-  faceAPI = ml5.faceApi(video,faceOptions,faceReady)
+  video.hide()
+  loadFaceModel()
 }
 
-function faceReady(){
-  faceAPI.detect(gotFaces)
+async function loadFaceModel() {
+  model = await blazeface.load()
 }
 
-function gotFaces(error, result){
-  if(error){
-    console.log(error)
-    return
+function draw(){
+  if(video.loadmetadata && model !== undefined){
+    getFace()
   }
-  detections = result;
-  faceAPI.detect(gotFaces)
-}
 
-function draw() {
-  background(0);
+  if(face !== undefined){
+    image(video,0,0,width,height)
 
-  if(detections.length > 0) {
-    let points = detections[0].landmarks.positions;
-    for(let i=0; i < points.length; i++){
-      stroke(161,95,251)
-      strokeWeight(4)
-      point(points[i]._x,points[i]._y)
+    if(firstFace){
+      console.log(face)
+      firstFace = false
     }
+    let rightEye = face.landmarks[0]
+    let leftEye = face.landmarks[1]
+    let nose = face.landmarks[2]
+    let rightEar = face.landmarks[4]
+    let leftEar = face.landmarks[5]
+
+    rightEye = scalePoint(rightEye)
+    leftEye = scalePoint(leftEye)
+    nose = scalePoint(nose)
   }
+
+  // Tegn her!
+  fill(255);
+  noStroke();
+  circle(leftEye.x,  leftEye.y,  40);     // eyeball
+  circle(rightEye.x, rightEye.y, 40);
+  fill(0);
+  circle(leftEye.x,  leftEye.y,  15);     // pupil
+  circle(rightEye.x, rightEye.y, 15);
+  fill(220);
+  circle(leftEye.x+2,  leftEye.y-2,  5);  // reflection
+  circle(rightEye.x+2, rightEye.y-2, 5);
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth,windowHeight)
+function scalePoint(pt){
+  let x = map(pt[0],0,video.width,0,width)
+  let y = map(pt[1],0,video.height,0,height)
+  return createVector(x,y)
+}
+
+async function getFace() {
+  const predictions = await model.estimateFaces(
+    document.querySelector('video'),
+    false
+  )
+  if(predictions.length === 0){
+    face = undefined
+  }
+
+  else{
+    face = predictions[0]
+  }
 }
